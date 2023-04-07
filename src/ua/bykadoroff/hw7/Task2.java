af44c0c14f4a409ff769ccf66d7825738b1a2919
bykadoroff <bykadoroff@gmail.com>
2023-04-07 22:44:13 +0300

    homework 7 is updated

--- src/ua/bykadoroff/hw7/Task2.java
+++ src/ua/bykadoroff/hw7/Task2.java
@@ -1,10 +1,20 @@
 package ua.bykadoroff.hw7;
 
 import static ua.bykadoroff.hw7.Vector.arrayVectors;
+import static ua.bykadoroff.hw7.Vector.showVectors;
 
 public class Task2 {
     public static void main(String[] args) {
-          arrayVectors(6);
 
+        Vector olenka = new Vector(-33,4,87);
+        Vector thisWay = new Vector(45,42,-687);
+        System.out.println(olenka.vectorProduct(thisWay));
+        System.out.println(olenka.cosVector(thisWay));
+        System.out.println(olenka.sumVector(thisWay));
+        System.out.println(olenka.differenceVector(thisWay));
+
+        System.out.println();
+
+        showVectors(arrayVectors(6));
     }
 }
--- src/ua/bykadoroff/hw7/Vector.java
+++ src/ua/bykadoroff/hw7/Vector.java
@@ -30,43 +30,47 @@ public class Vector {
                 '}';
     }
 
-    public Vector vectorProduct(Vector vector1, Vector vector2) {
-        double x = vector1.y * vector2.z - vector1.z * vector2.y;
-        double y = vector1.z * vector2.x - vector1.x * vector2.z;
-        double z = vector1.x * vector2.y - vector1.y * vector2.x;
+    public Vector vectorProduct(Vector vector) {
+        double x = this.y * vector.z - this.z * vector.y;
+        double y = this.z * vector.x - this.x * vector.z;
+        double z = this.x * vector.y - this.y * vector.x;
         return new Vector(x, y, z);
     }
 
-    public double cosVector(Vector vector1, Vector vector2) {
-        return (vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z)
-                / (Math.sqrt(vector1.x * vector1.x + vector1.y * vector1.y + vector1.z * vector1.z)) *
-                (Math.sqrt(vector2.x * vector2.x + vector2.y * vector2.y + vector2.z * vector2.z));
+    public double cosVector(Vector vector) {
+        return (this.x * vector.x + this.y * vector.y + this.z * vector.z)
+                / (Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)) *
+                (Math.sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z));
     }
 
-    public Vector sumVector(Vector vector1, Vector vector2) {
-        double x = vector1.x + vector2.x;
-        double y = vector1.y + vector2.y;
-        double z = vector1.z + vector2.z;
+    public Vector sumVector(Vector vector) {
+        double x = this.x + vector.x;
+        double y = this.y + vector.y;
+        double z = this.z + vector.z;
         return new Vector(x, y, z);
     }
 
-    public Vector differenceVector(Vector vector1, Vector vector2) {
-        double x = vector1.x - vector2.x;
-        double y = vector1.y - vector2.y;
-        double z = vector1.z - vector2.z;
+    public Vector differenceVector(Vector vector) {
+        double x = this.x - vector.x;
+        double y = this.y - vector.y;
+        double z = this.z - vector.z;
         return new Vector(x, y, z);
     }
 
-    public static Vector [] arrayVectors (int N){
-        Vector [] array = new Vector [N];
-        for(int i = 0; i < array.length; i ++){
-            double x = ThreadLocalRandom.current().nextDouble(-100.0,100.0);
-            double y = ThreadLocalRandom.current().nextDouble(-100.0,100.0);
-            double z = ThreadLocalRandom.current().nextDouble(-100.0,100.0);
-            array [i] = new Vector (x,y,z);
-            System.out.println(array[i]);
+    public static Vector[] arrayVectors(int N) {
+        Vector[] array = new Vector[N];
+        for (int i = 0; i < array.length; i++) {
+            double x = ThreadLocalRandom.current().nextDouble(-100.0, 100.0);
+            double y = ThreadLocalRandom.current().nextDouble(-100.0, 100.0);
+            double z = ThreadLocalRandom.current().nextDouble(-100.0, 100.0);
+            array[i] = new Vector(x, y, z);
         }
         return array;
     }
 
+    public static void showVectors(Vector[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
