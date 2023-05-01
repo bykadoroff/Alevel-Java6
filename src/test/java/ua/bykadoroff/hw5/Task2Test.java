afb66fbc1e6e4cccc38341958fc41eaf70079ce7
bykadoroff <ruslanbykadoroff@gmail.com>
2023-05-01 22:53:53 +0300

    on 2 hours ago feedback

--- src/test/java/ua/bykadoroff/hw5/Task2Test.java
+++ src/test/java/ua/bykadoroff/hw5/Task2Test.java
@@ -7,24 +7,24 @@ public class Task2Test {
     @Test
     public void testCheckForSortWithSortedArray() {
         int[] arraySorted = {1, 2, 3, 4, 5};
-        assertEquals(true,Task2.checkForSort(arraySorted));
+        assertTrue(Task2.checkForSort(arraySorted));
     }
 
     @Test
     public void testCheckForSortWithUnsortedArray() {
         int[] arrayUnsorted = {2, 5, 1, 4, 3};
-        assertEquals(false,Task2.checkForSort(arrayUnsorted));
+        assertFalse(Task2.checkForSort(arrayUnsorted));
     }
 
     @Test
     public void testCheckForSortWithTheSameNumbersArray() {
         int[] arrayTheSameNumbers = {2, 2, 2, 2, 2};
-        assertEquals(true,Task2.checkForSort(arrayTheSameNumbers));
+        assertTrue(Task2.checkForSort(arrayTheSameNumbers));
     }
 
     @Test
     public void testCheckForSortWithEmptyArray() {
         int[] arrayEmpty = new int[0];
-        assertEquals(true,Task2.checkForSort(arrayEmpty));
+        assertTrue(Task2.checkForSort(arrayEmpty));
     }
 }
