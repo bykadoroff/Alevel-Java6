d01416b784ffb2a452da12ca98920c6e54d53a1b
bykadoroff <ruslanbykadoroff@gmail.com>
2023-05-03 15:57:15 +0300

    hw 10, Student class for Test added

--- src/test/java/ua/bykadoroff/hw10/AspirantTest.java
+++ src/test/java/ua/bykadoroff/hw10/AspirantTest.java
@@ -6,40 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AspirantTest {
 
-    // Tests for getScholarship method for Student class
-    @Test
-    void testGetScholarshipWithEmptyAverageMarkForStudentClass() {
-
-        Student hans = new Student();
-        hans.firstName = "Hans";
-        hans.lastName = "Shultez";
-        hans.group = "Group 1";
-
-        assertEquals(80, hans.getScholarship());
-    }
-
-    @Test
-    void testGetScholarshipWith5AverageMarkForStudentClass() {
-        Student hans = new Student("Hans", "Shultez", "Group 1", 5);
-
-        assertEquals(100, hans.getScholarship());
-    }
-
-    @Test
-    void testGetScholarshipWithLess5AverageMarkForStudentClass() {
-        Student hans = new Student("Hans", "Shultez", "Group 1", 3.3);
-
-        assertEquals(80, hans.getScholarship());
-    }
-
-    @Test
-    void testGetScholarshipWithZeroAverageMarkForStudentClass() {
-        Student hans = new Student("Hans", "Shultez", "Group 1", 0);
-
-        assertEquals(80, hans.getScholarship());
-    }
-
-    // Tests for getScholarship method for Aspirant class
     @Test
     void testGetScholarshipWithEmptyAverageMarkForAspirantClass() {
 
